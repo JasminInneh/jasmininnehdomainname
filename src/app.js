5458f292a5f5fa9d986e654a797c848d3a09e825
Jasmin Inneh <110208655+JasminInneh@users.noreply.github.com>
2023-12-08 23:16:21 +0000

    domain name

--- src/app.js
+++ src/app.js
@@ -7,6 +7,28 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-  console.log("test")
+  let randomNumber = Math.random() * 10;
+  // document.querySelector("#newDomainBTN").addEventListener("click", () => {
+  // document.querySelector("#the-domain-name").innerHTML = generateDomain();
+  // });
 };
+
+let generateDomain = () => {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let containerselect = document.querySelector(".domain-names");
+  for (let k = 0; k < 2; k++) {
+    for (let i = 0; i < 2; i++) {
+      for (let j = 0; j < 2; j++) {
+        console.log(pronoun[k] + adj[i] + noun[j] + ".com");
+        let newChild = document.createElement("h1");
+        newChild.innerText = pronoun[k] + adj[i] + noun[j] + ".com";
+        containerselect.append(newChild);
+      }
+    }
+  }
+};
+console.log(generateDomain());
+
+
